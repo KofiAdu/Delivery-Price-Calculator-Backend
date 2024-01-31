404dd13da5d37db029b1ca5c6712e8059c1510c8
KofiAdu <72742298+KofiAdu@users.noreply.github.com>
2024-01-31 15:13:01 +0200

    New update

--- deliveryfeeapi/tests.py
+++ deliveryfeeapi/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import unittest
+from deliveryfeeapi.views import DeliveryFeeView
 
 # Create your tests here.
+class TestDeliveryFeeView(unittest.TestCase):
+
+    def setUp(self):
+        self.delivery_fee_calculator = DeliveryFeeView()
+
+    def test_calculate_delivery_fee(self):
+        cart_value = 790
+        delivery_distance = 2235
+        number_of_items = 4
+        time = "2024-01-15T13:00:00Z"
+        
+        if 'Z' in time:
+            time = time.rstrip('Z')
+
+        expected_fee = self.delivery_fee_calculator.calculate_delivery_fee(cart_value, delivery_distance, number_of_items, time)
+
+        self.assertEqual(expected_fee, 710)
+
+if __name__ == '__main__':
+    unittest.main()
--- deliveryfeeapi/urls.py
+++ deliveryfeeapi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import calculate_delivery_fee_api
+from deliveryfeeapi.views import DeliveryFeeView
+
+#create an isnstance of the DeliveryFeeView
+delivery_fee_view = DeliveryFeeView()
 
 urlpatterns = [
-    path('calculate_delivery_fee_api/', calculate_delivery_fee_api, name='calculate_delivery_fee_api'),
+    path('calculate_delivery_fee_api/', delivery_fee_view.calculate_delivery_fee_api, name='calculate_delivery_fee_api'),
 ]
